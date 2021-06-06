9609ea693eb7611355dc89e9ff2ef09e6d372ee0
Susant Sahani <ssahani@vmware.com>
2021-06-07 01:22:03 +0200

    DHClient: Introduce parser for lease

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -4,10 +4,29 @@
 package parser
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"net"
+	"os"
 	"strings"
+
+	"github.com/network-event-broker/pkg/conf"
 )
+
+type Lease struct {
+	Interface    string
+	Address      string
+	ServerName   string
+	SubnetMask   string
+	Routers      string
+	LeaseTime    string
+	Server       string
+	Hostname     string
+	Dns          []string
+	DomainSearch []string
+	Domain       []string
+}
 
 func ParseIP(ip string) (net.IP, error) {
 	if len(ip) == 0 {
@@ -30,4 +49,60 @@ func ParseDNS(line string) []string {
 	s = strings.Replace(s, ",", " ", -1)
 
 	return strings.SplitAfter(s, " ")
+}
+
+func ParseDHClientLease() (map[string]*Lease, error) {
+	file, err := os.OpenFile(conf.DHClientLeaseFile, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	leases := make(map[string]*Lease)
+
+	var lease *Lease
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+		line := strings.TrimSpace(scanner.Text())
+
+		if strings.HasPrefix(line, "lease ") && strings.HasSuffix(line, " {") {
+			lease = new(Lease)
+			continue
+		}
+
+		if strings.HasSuffix(line, "}") {
+			leases[lease.Interface] = lease
+			continue
+		}
+
+		switch {
+		case strings.Contains(line, "interface"):
+			lease.Interface = strings.Split(line, "\"")[1]
+		case strings.Contains(line, "fixed-address"):
+			lease.Address = strings.TrimSuffix(strings.Split(line, " ")[1], ";")
+		case strings.Contains(line, "subnet-mask"):
+			lease.SubnetMask = strings.TrimSuffix(strings.Split(line, " ")[2], ";")
+		case strings.Contains(line, "routers"):
+			lease.Routers = strings.TrimSuffix(strings.Split(line, " ")[2], ";")
+		case strings.Contains(line, "dhcp-server-identifier"):
+			lease.Server = strings.TrimSuffix(strings.Split(line, " ")[2], ";")
+		case strings.Contains(line, "domain-name-servers"):
+			lease.Dns = strings.Split(strings.TrimSuffix(strings.Split(line, " ")[2], ";"), ",")
+			fmt.Println(lease.Dns)
+		case strings.Contains(line, "domain-name"):
+			s := strings.TrimSuffix(strings.ReplaceAll(line, "option domain-name", ""), ";")
+			s = strings.ReplaceAll(s, ",", "")
+			lease.Domain = strings.Split(s, "\"")
+		case strings.Contains(line, "host-name"):
+			lease.Hostname = strings.Split(line, "\"")[1]
+		case strings.Contains(line, "domain-search"):
+			s := strings.TrimSuffix(strings.ReplaceAll(line, "option domain-search", ""), ";")
+			s = strings.ReplaceAll(s, ",", "")
+			lease.DomainSearch = strings.Split(s, "\"")
+		}
+
+	}
+
+	return leases, nil
 }
