1df7acdf7adaeef408c003f8a9cc5e6f7137c576
Susant Sahani <ssahani@vmware.com>
2022-11-25 17:49:48 +0530

    Add HW properties

--- listeners/networkd.go
+++ listeners/networkd.go
@@ -38,16 +38,48 @@ const (
 )
 
 type LinkDescribe struct {
-	AddressState     string   `json:"AddressState"`
-	AlternativeNames []string `json:"AlternativeNames"`
-	CarrierState     string   `json:"CarrierState"`
-	Driver           string   `json:"Driver"`
-	IPv4AddressState string   `json:"IPv4AddressState"`
-	IPv6AddressState string   `json:"IPv6AddressState"`
-	Index            int      `json:"Index"`
+	Index            int                     `json:"Index"`
+	Mtu              int                     `json:"MTU"`
+	TxQLen           int                     `json:"TxQLen"`
+	Name             string                  `json:"Name"`
+	AlternativeNames string                  `json:"AlternativeNames"`
+	HardwareAddr     string                  `json:"HardwareAddr"`
+	Flags            string                  `json:"Flags"`
+	RawFlags         uint32                  `json:"RawFlags"`
+	ParentIndex      int                     `json:"ParentIndex"`
+	MasterIndex      int                     `json:"MasterIndex"`
+	Namespace        string                  `json:"Namespace"`
+	Alias            string                  `json:"Alias"`
+	Statistics       *netlink.LinkStatistics `json:"Statistics"`
+
+	Promisc int `json:"Promisc"`
+	Xdp     struct {
+		Fd       int  `json:"Fd"`
+		Attached bool `json:"Attached"`
+		Flags    int  `json:"Flags"`
+		ProgID   int  `json:"ProgId"`
+	} `json:"Xdp"`
+	EncapType   string `json:"EncapType"`
+	Protinfo    string `json:"Protinfo"`
+	OperState   string `json:"OperState"`
+	NetNsID     int    `json:"NetNsID"`
+	NumTxQueues int    `json:"NumTxQueues"`
+	NumRxQueues int    `json:"NumRxQueues"`
+	GSOMaxSize  uint32 `json:"GSOMaxSize"`
+	GSOMaxSegs  uint32 `json:"GSOMaxSegs"`
+	Group       uint32 `json:"Group"`
+	Slave       string `json:"Slave"`
+
+	KernelOperState string `json: "KernelOperState"`
+
+	AddressState     string `json:"AddressState"`
+	CarrierState     string `json:"CarrierState"`
+	Driver           string `json:"Driver"`
+	IPv4AddressState string `json:"IPv4AddressState"`
+	IPv6AddressState string `json:"IPv6AddressState"`
+
 	LinkFile         string   `json:"LinkFile"`
 	Model            string   `json:"Model"`
-	Name             string   `json:"Name"`
 	OnlineState      string   `json:"OnlineState"`
 	OperationalState string   `json:"OperationalState"`
 	Path             string   `json:"Path"`
@@ -67,9 +99,29 @@ type LinksDescribe struct {
 
 func fillOneLink(link netlink.Link) *LinkDescribe {
 	l := LinkDescribe{
-		Index: link.Attrs().Index,
-		Name:  link.Attrs().Name,
-		Type:  link.Attrs().EncapType,
+
+		Type:            link.Attrs().EncapType,
+		KernelOperState: link.Attrs().OperState.String(),
+		Index:           link.Attrs().Index,
+		Mtu:             link.Attrs().MTU,
+		TxQLen:          link.Attrs().TxQLen,
+		Name:            link.Attrs().Name,
+		HardwareAddr:    link.Attrs().HardwareAddr.String(),
+		RawFlags:        link.Attrs().RawFlags,
+		ParentIndex:     link.Attrs().ParentIndex,
+		MasterIndex:     link.Attrs().MasterIndex,
+		Alias:           link.Attrs().Alias,
+		EncapType:       link.Attrs().EncapType,
+		OperState:       link.Attrs().OperState.String(),
+		NetNsID:         link.Attrs().NetNsID,
+		NumTxQueues:     link.Attrs().NumTxQueues,
+		NumRxQueues:     link.Attrs().NumRxQueues,
+		GSOMaxSize:      link.Attrs().GSOMaxSize,
+		GSOMaxSegs:      link.Attrs().GSOMaxSegs,
+		Group:           link.Attrs().Group,
+		Statistics:      link.Attrs().Statistics,
+		Promisc:         link.Attrs().Promisc,
+		Flags:           link.Attrs().Flags.String(),
 	}
 
 	l.AddressState, _ = ParseLinkAddressState(link.Attrs().Index)
